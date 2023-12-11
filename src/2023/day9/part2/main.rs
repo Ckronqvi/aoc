a8ea6ed7010db4dacd588e8232dfde50c390a2e9
Ckronqvi21 <ckronqvi21@student.oulu.fi>
2023-12-11 20:53:55 +0200

    day9

---
+++ src/2023/day9/part2/main.rs
@@ -0,0 +1,50 @@
+
+fn main() {
+    let input = std::fs::read_to_string("src/input.txt").unwrap();
+    let mut all_numbers: Vec<Vec<i64>> = Vec::new();
+
+    for line in input.lines() {
+        all_numbers.push(
+            line.split_ascii_whitespace()
+                .map(|c| c.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>()
+        );
+    }
+
+    let mut total: i64 = 0;
+    for row in all_numbers {
+        total += count_next(row);
+    }
+
+    print!("Total {}", total);
+}
+
+fn count_next(numbers: Vec<i64>) -> i64 {
+    let mut extrapolated_lines: Vec<Vec<i64>> = Vec::new();
+    let mut temp: Vec<i64> = numbers.clone();
+    extrapolated_lines.push(numbers);
+    loop {
+        temp = get_next_line(&temp);
+        if temp.iter().sum::<i64>() == 0 {
+            break;
+        }
+        extrapolated_lines.push(temp.clone());
+    }
+
+    let mut i: i32 = (extrapolated_lines.len()-1) as i32;
+    let mut prev: i64 = 0i64;
+    while i >= 0 {
+        prev = extrapolated_lines[i as usize][0] - prev;
+        i -= 1;
+    }
+    prev
+}
+
+fn get_next_line(numbers: &Vec<i64>) -> Vec<i64> {
+    let mut new_numbers: Vec<i64> = Vec::new();
+
+    for n in 0..numbers.len()-1 {
+        new_numbers.push(numbers[n+1] - numbers[n]);
+    }
+    new_numbers
+} 
